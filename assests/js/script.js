c79ae837d1c3007507e43a6fccefb10978803b70
Asmaa <Asmaa99musse@gmail.com>
2021-10-16 21:18:28 +0100

    created time slots from 9am to 5pm

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,15 @@
 // Create an array for the displayed times
-const time = [""];
+const time = moment();
+const time = ["9, 10, 11, 12, 13, 14, 15, 16, 17"];
 
 // Get current date for the header using moment.js
+
+const currentTime = moment().format("dddd, MMMM Do YYYY");
+
+const getCurrentDate = function () {
+  // get current date using moment.js
+  //
+};
 
 // create an if else statement for the past and present times
 
