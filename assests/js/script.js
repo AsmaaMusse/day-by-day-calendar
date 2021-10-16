5a8b27e614712a2cd9924765aef426712e781883
Asmaa <Asmaa99musse@gmail.com>
2021-10-16 22:19:11 +0100

    declared hr vairables in js

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,14 @@
 // Create an array for the displayed times
-const time = moment();
 const time = ["9, 10, 11, 12, 13, 14, 15, 16, 17"];
+const hr9 = $("#9");
+const hr10 = $("10");
+const hr11 = $("11");
+const hr12 = $("12");
+const hr13 = $("13");
+const hr14 = $("14");
+const hr15 = $("15");
+const hr16 = $("16");
+const hr17 = $("17");
 
 // Get current date for the header using moment.js
 
