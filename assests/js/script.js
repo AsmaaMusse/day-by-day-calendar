07fb8c07c30e8b278c52f314da03c95aa9e62da4
Asmaa <Asmaa99musse@gmail.com>
2021-10-17 15:58:54 +0100

    creating a function for currentTime

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,29 +1,32 @@
-// Create an array for the displayed times
-// const time = ["9, 10, 11, 12, 13, 14, 15, 16, 17"];
-const hr9 = $("#9");
-const hr10 = $("10");
-const hr11 = $("11");
-const hr12 = $("12");
-const hr13 = $("13");
-const hr14 = $("14");
-const hr15 = $("15");
-const hr16 = $("16");
-const hr17 = $("17");
-
 // Get current date for the header using moment.js
 const displayDate = moment();
 $("#currentDay").text(moment().format("dddd, MMMM Do HH:mm"));
 
+// Get current time for the if else statement
+function currentTime() {
+  const current = moment().hours();
+  const time = $(".time-block");
+
+  console.log(current);
+
+  time.each(function () {
+    // parse string and return integer
+    const hour = parseInt($(this).attr("id"));
+
+    console.log(hour);
+  });
+}
+
 // create an if else statement for the past and present times
-const timeBlock = function () {
-  if (condition) {
-    // if time block === display present colours.
-  } else if (condition) {
-    // Else if time block === display present colours.
-  } else {
-    // if time block selected is > current time, display future colours.
-  }
-};
+// const timeBlock = function () {
+//   if (timeBlock === currentTime) {
+//     // if time block === display present colours.
+//   } else if (timeBlock < currentTime) {
+//     // Else if time block === display present colours.
+//   } else {
+//     // if time block selected is > current time, display future colours.
+//   }
+// };
 
 // save plans to local storage
 
